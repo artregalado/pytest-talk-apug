f0b8c75d976e15eaaa695ef9b57d6f9d607bcd3f
ormiret <rmcw@allmail.net>
2023-12-13 13:35:58 +0000

    Use full path for requirements.txt

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-
+pwd
 sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6  -y
-pip install -r ../requirements.txt
+pip install -r /worksapces/pytest-talk-apug/requirements.txt
